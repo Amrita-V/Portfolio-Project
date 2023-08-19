623b7a4ded495c8ec600afba2e54d834174af4f4
Amrita-V <amritav2981@gmail.com>
2023-08-19 18:12:36 +0530

    Set styling and branding for font, theme color and button style.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Introduction from "./components/intro-header/Introduction";
+import Projects from "./components/projects/Projects";
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
     <div>
     <h1>Hello </h1>
+    <button className="primary-button">Click</button>
+    <button className="secondary-button">Click</button>
+
+    <Topbar/>
+    <Introduction/>
+    <Projects/>
+    <Contact/>
+    <Footer/>
     </div>
   );
 }
